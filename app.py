e0baaec46e74c338f4b203b14580d450ce32d0fb
lightingproject <shakeralebr26@gmail.com>
2026-08-25 22:51:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def technician_logout():
     return redirect(url_for('technician_login', pole_id=pole_id))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
